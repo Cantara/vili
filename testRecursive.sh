cba7c7e0e2fcd950e3493a74f3d5f51ebee11785
Sindre Brurberg <sindre@brurberg.no>
2022-03-09 16:29:52 +0100

    Fixed recursive test script

--- testRecursive.sh
+++ testRecursive.sh
@@ -5,7 +5,9 @@ recursive_for_loop() {
         if [ -d $f  -a ! -h $f ]; then
             #echo $f
             cd -- "$f"
-            go test
+            if find "." -maxdepth 1 | grep "_test.go"; then
+                go test
+            fi
             recursive_for_loop
             cd ..
         fi
